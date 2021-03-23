72de765f4337612df2d39a78d0531cbd2afbd42f
Vladyslav Herasymchuk <slaughternal@gmail.com>
2021-03-23 12:05:48 +0200

    Added join basic

--- ep.js
+++ ep.js
@@ -16,6 +16,11 @@
 
 const SQLParser = require('./lib/SQLParser.js');
 
-const r = SQLParser.parseSQL("select * from customers INNER JOIN films ON `customers.id` = `films.id`", 'aggregate');
+// const r = SQLParser.parseSQL("select * from customers INNER JOIN films ON `customers.id` = `films.id` INNER JOIN films ON `films.id` = `stores.id`", 'aggregate');
+
+// const r = SQLParser.parseSQL("select `Address.City` as City,avg(size(`Rentals`)) as AvgRentals from `customers` where `First Name` like 'm%' group by `Address.City`", 'aggregate');
+
+const r = SQLParser.parseSQL("select `collA.field3`, `collB.field3`, count(*) from collA inner join collB on `collA.field1` = `collB.field2` where `collA.date` > 12 group by `collA.field3`, `collB.field3` having count(*) > 250 order by `collA.field3`, `collB.field3`", 'aggregate');
+
 
 console.log(JSON.stringify(r, null, 2))
