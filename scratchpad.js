2b390a705e80ee8a07ffe466af8f34851475fe87
Martin Naude <martin@synatic.com>
2021-03-24 11:28:56 +0200

    added some comments

--- scratchpad.js
+++ scratchpad.js
@@ -1,21 +1,5 @@
-const {MongoClient} = require('mongodb');
-const assert = require('assert');
+const SQLParser=require('./lib/SQLParser');
 
+let parsedVal=SQLParser.parseSQLtoAST("select a,b,unwind(c) as c,c.a,c.b from `table` as t1  inner join `table2` as t2  on a=b ")
+let x;
 
-const _connectionString="mongodb://127.0.0.1:27017";
-const _dbName="sql-to-mongo-test";
-
-async function run (){
-    try{
-        const client = new MongoClient(_connectionString);
-        await client.connect();
-        const dbs=await client.listDatabases().toArray();
-        let xc;
-    }catch(exp){
-        console.error(exp)
-    }
-
-
-
-}
-run();
