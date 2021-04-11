5ea0b403ace35bf83eb7998d7e13f506bafa81de
Martin Naude <martin@synatic.com>
2021-04-11 21:57:43 +0200

    tweaking mongo functions

--- scratchpad.js
+++ scratchpad.js
@@ -1,5 +1,5 @@
 const SQLParser=require('./lib/SQLParser');
 
-let parsedVal=SQLParser.makeMongoQuery("select log10(3) * floor(a) as s from collection")
+let parsedVal=SQLParser.makeMongoQuery("select (log10(3) * floor(a) +1) as s from collection")
 let x;
 
