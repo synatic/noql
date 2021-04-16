a86c7d36c32ea576ebc264d942d345fa1cedaf60
Vladyslav Herasymchuk <vladyslav.herasymchuk@limestonedigital.com>
2021-04-16 14:57:00 +0300

    Fixed client Arithmetic Expression Operators exec

--- test/Client.js
+++ test/Client.js
@@ -117,19 +117,43 @@ describe('Client Queries', function () {
 
 
     describe('Arithmetic Expression Operators', function () {
-        for (const [key, test] of Object.entries(arithmeticExpressionOperators.tests)) {
-            it(key, async function () {
-                try {
-                    const parsedQuery = SQLParser.parseSQL(test.query, test.type);
-                    let results = await client.db(_dbName).collection(parsedQuery.collections[0]).aggregate(parsedQuery.pipeline);
-                    results = await results.toArray()
+        (async () => {
+            const tests = _queryTests.filter(q => !!q.query && !q.error);
+            for (const [key, test] of Object.entries(arithmeticExpressionOperators.tests)) {
+                it(key,function(done){
+                    (async () => {
+                        try {
+                            const parsedQuery = SQLParser.parseSQL(test.query);
+                            if (parsedQuery.pipeline){
+                                let results = await client.db(_dbName).collection(parsedQuery.collections[0]).aggregate(parsedQuery.pipeline);
+                                results = await results.toArray()
 
-                    console.log(`\u2714 ${test.query} | count:${results.length} | ${results[0] ? JSON.stringify(results[0]) : ""}`);
-
-                } catch (exp) {
-                    console.error(`\u2716 ${test.query} ${exp.message || ""}`);
-                }
-            });
-        }
+                                console.log(`\u2714 ${test.query} | count:${results.length} | ${results[0] ? JSON.stringify(results[0]) : ""}`);
+                            } else {
+                                if (parsedQuery.count) {
+                                    const count = await client.db(_dbName).collection(parsedQuery.collection).countDocuments(parsedQuery.query || null);
+                                    console.log(`${count}`);
+                                } else {
+                                    let find = client.db(_dbName).collection(parsedQuery.collection).find(parsedQuery.query || null, {projection: parsedQuery.projection});
+                                    if (parsedQuery.sort) {
+                                        find.sort(parsedQuery.sort)
+                                    }
+                                    if (parsedQuery.limit) {
+                                        find.limit(parsedQuery.limit)
+                                    }
+                                    const results = await find.toArray();
+                                    console.log(`count:${results.length} | ${results[0] ? JSON.stringify(results[0]) : ""}`);
+                                }
+                            }
+                            
+                            done();
+                        } catch (exp) {
+                            done(exp ? exp.message : null);
+                        }
+                    })()
+                });
+            }
+            done();
+        })();
     });
 });
