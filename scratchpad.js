026405cb2f9ab461622aa529df620dd1018f8122
Martin Naude <martin@synatic.com>
2021-08-03 09:44:14 +0200

    tweaking lookup logic

--- scratchpad.js
+++ scratchpad.js
@@ -11,17 +11,16 @@ const SQLParser=require('./lib/SQLParser');
 // const parsedVal=SQLParser.makeMongoAggregate("select  `customer-notes`.*,YEAR(DATE_FROM_STRING(`date`)) as year from `customer-notes`")
 // select * from `customers` where id in (select id from `customer notes` where abs(id) <10)
 // const parsedVal=SQLParser.makeMongoAggregate("select *,FIRST_IN_ARRAY(inventory) as inventory_docs from orders inner join `inventory` on sku=item")
-const parsedVal=SQLParser.makeMongoAggregate("select *,FIRST_IN_ARRAY(inventory) as inventory_docs from orders inner join `inventory` on orders.item=inventory.sku")
-
+// const parsedVal=SQLParser.makeMongoAggregate("select *,FIRST_IN_ARRAY(inventory) as inventory_docs from orders inner join `inventory` on orders.item=inventory.sku")
 
 // let parsedVal=SQLParser.makeMongoAggregate("select * from `customers` where id in (select id from `customer notes` where abs(id) <10) and id in (select id from `customer notes` where abs(id) <10)")
-// const parsedVal=SQLParser.makeMongoAggregate("select *,UNWIND(inventory) as inventory_docs from orders inner join `inventory` on orders.item=inventory.sku")
-
+const parsedVal=SQLParser.makeMongoAggregate("select c.*,cn.*,cn2.*  from customers c inner join `customer-notes` cn on c.id=cn.id inner join `customer-notes2` cn2 on cn2.id=c.id")
+// const parsedVal=SQLParser.makeMongoAggregate("Select VoucherName,Status,Sum(1) as Count,case when Status='Created' then 1 else 0 end as SortOrder from test group by VoucherName,Status order by VoucherName,Status")
 // let parsedVal=SQLParser.makeMongoAggregate("select `Address.City` as City,abs(-1) as absId,avg(lengthOfArray(`Rentals`)) as AvgRentals from `customers` where `First Name` like 'm%' and absId >1 group by `Address.City`,absId")
 // const parsedVal=SQLParser.makeMongoAggregate("select `First Name`,`Address.City` as City,abs(-1) as absId from `customers` where `First Name` like 'm%' and abs(-1) >=1")
 
 // let parsedVal=SQLParser.makeMongoAggregate("select * from (select id,`First Name`,`Last Name`,lengthOfArray(Rentals,'id') from customers )")
-// let parsedVal=SQLParser.makeMongoAggregate("select `Address.Country` as Country,sum(id) as totalId from customers group by `Address.Country`")
+// const parsedVal=SQLParser.makeMongoAggregate("select `Address.Country` as Country,sum(id) as totalId from customers group by `Address.Country`")
 
 
 return console.log(JSON.stringify(parsedVal.pipeline?parsedVal.pipeline:parsedVal.query))
