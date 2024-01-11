7de54a58a5862c27b045f2970b8812b4c439fa2d
Ryan Kotzen <ryan@synatic.com>
2024-01-11 14:43:52 +0200

    Fix for likes in on statements

--- TODO.md
+++ TODO.md
@@ -1,11 +1,14 @@
 # Todos
 
+-   [High] Fix bug with not being able to do a like in an ON clause
+-   [High] Monitoring dashboard for failed parses of buffer queries
+-   [High] support for except
+-   [High] support for intersect
 -   [High] ntile need to fix ast
 -   [High] Update docs to not specify db
--   [High] Update docs to have except, intersect
+-   [High] Update docs to have except, intersect, ntile
 -   [High] Check for skipped tests
 -   [Medium] Test out the library with sveltkit, seems to have issues in web
--   [Medium] Monitoring dashboard for failed parses of buffer queries
 -   [Low] Update intellisense
 -   [Low] Deprecate ignoreDateValues
 -   [Low] orderDate < CURRENT_DATE() includes nulls
--- test/bug-fix-tests/bug-fix.test.js
+++ test/bug-fix-tests/bug-fix.test.js
@@ -908,40 +908,17 @@ describe('bug-fixes', function () {
                 mode,
             });
         });
-        it.skip('should allow you to use the like clause in an on', async () => {
+        it('should allow you to use the like clause in an on', async () => {
             const queryString = `
-                SELECT  *
+                SELECT  *,unset(_id,company._id,AMSCompany._id,company.orderDate)
                 FROM (SELECT *, item as CName from orders) "company|first"
                 LEFT JOIN (select * from inventory) "AMSCompany|first"
                     ON TO_STRING(sku) LIKE company.CName
             `;
-            const $lookup = {
-                from: 'inventory',
-                as: 'AMSCompany',
-                let: {
-                    regex: '$company.CName',
-                },
-                pipeline: [
-                    {
-                        $match: {
-                            $expr: {
-                                $regexMatch: {
-                                    input: {
-                                        $toString: '$sku',
-                                    },
-                                    regex: '$$regex',
-                                    options: 'i',
-                                },
-                            },
-                        },
-                    },
-                ],
-            };
             await queryResultTester({
                 queryString: queryString,
                 casePath: 'scratchpad.case2',
-                mode: 'write',
-                outputPipeline: true,
+                mode,
             });
         });
     });
