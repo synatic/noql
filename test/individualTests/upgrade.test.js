5a43ac6c1475a5212834e613c598200a9a051063
Ryan Kotzen <ryankotzen@Ryans-MacBook-Pro.local>
2024-09-03 15:35:28 +0200

    feat(unpivot): start of logic

--- test/individualTests/upgrade.test.js
+++ test/individualTests/upgrade.test.js
@@ -427,40 +427,22 @@ describe('node-sql-parser upgrade tests', function () {
             });
         });
 
-        describe.skip('UNPIVOT', () => {
+        describe('UNPIVOT', () => {
             it('should unpivot employee columns to rows', async () => {
                 const queryText = `
-                    SELECT VendorID, Employee, Orders
+                    SELECT *
                     FROM (
                         SELECT VendorID, Emp1, Emp2, Emp3, Emp4, Emp5
-                        FROM (
-                            SELECT VendorID,
-                                [250] AS Emp1,
-                                [251] AS Emp2,
-                                [256] AS Emp3,
-                                [257] AS Emp4,
-                                [260] AS Emp5
-                            FROM (
-                                SELECT PurchaseOrderID,
-                                EmployeeID, VendorID
-                                FROM Purchasing_PurchaseOrderHeader
-                            ) p
-                            PIVOT (
-                                COUNT(PurchaseOrderID)
-                                FOR EmployeeID IN ([250], [251], [256], [257], [260])
-                            ) AS pvt
-                        ) p
-                    ) AS PivotTable
-                    UNPIVOT (
-                        Orders FOR Employee IN (Emp1, Emp2, Emp3, Emp4, Emp5)
-                    ) AS UnpivotTable
-                    ORDER BY VendorID, Employee
+                        FROM pvt
+                        GROUP BY DaysToManufacture, StandardCost
+                        ORDER BY DaysToManufacture, StandardCost
+                    ) 'unpvt|unpivot([Orders],Employee,[Emp1, Emp2, Emp3, Emp4, Emp5])'
                 `;
 
                 await queryResultTester({
                     queryString: queryText,
                     casePath: 'unpivot.case1',
-                    mode,
+                    mode: 'write',
                     outputPipeline: false,
                 });
             });
